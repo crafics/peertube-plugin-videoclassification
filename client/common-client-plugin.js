7432f245be8f279af5ac90b9a4769bbb1767abd9
Manfred Weber <manfred.weber@gmail.com>
2021-03-26 11:44:54 +0100

    Update common-client-plugin.js

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -1,5 +1,15 @@
 function register ({ registerHook, peertubeHelpers }) {
-
+peertubeHelpers.showModal({
+   title: 'My custom modal title',
+   content: '<p>My custom modal content</p>',
+   // Optionals parameters :
+   // show close icon
+   close: true,
+   // show cancel button and call action() after hiding modal
+   cancel: { value: 'cancel', action: () => {} },
+   // show confirm button and call action() after hiding modal
+   confirm: { value: 'confirm', action: () => {} },
+ })
 }
 
 export {
